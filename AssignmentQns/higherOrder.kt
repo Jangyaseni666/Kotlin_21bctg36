cbf3aa243ebf0ce1540280f8d087a1007e1e00df
Jangyaseni Sahoo <96827920+Jangyaseni666@users.noreply.github.com>
2024-06-13 14:50:37 +0530

    Made changes to higherorder.kt

--- AssignmentQns/higherOrder.kt
+++ AssignmentQns/higherOrder.kt
@@ -2,8 +2,19 @@ fun operation(a: Int, b: Int, op: (Int, Int) -> Int): Int {
     return op(a, b)
 }
 fun main() {
-    val add: (Int, Int) -> Int = { x, y -> x + y } // lambda
-    
-    var sum = operation(3,5,add)
-    println("The sum is ${sum}")
+    val add: (Int, Int) -> Int = { x, y -> x + y }
+
+    val multiply: (Int, Int) -> Int = { x, y -> x * y }
+
+    val sub: (Int, Int) -> Int = {x,y -> x-y}
+
+    val sum = operation(5, 3, add)
+    println("The sum is: $sum")
+
+    val product = operation(5, 3, multiply)
+    println("The product is: $product")
+
+    val difference = operation(5, 3, sub)
+    println("The difference is: $difference")
+
 }
