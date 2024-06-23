3f90d30f71c762d87a5fb7c3dae9905b252a40df
Jangyaseni Sahoo <96827920+Jangyaseni666@users.noreply.github.com>
2024-06-23 18:39:31 +0530

    Added June 23 assignment

--- app/src/main/java/com/example/androidsi/MainActivity.kt
+++ app/src/main/java/com/example/androidsi/MainActivity.kt
@@ -19,6 +19,8 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -31,8 +33,10 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.LiveData
@@ -49,6 +53,7 @@ import retrofit2.http.GET
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalFoundationApi::class)
     private val productVM: ProductViewModel by viewModels()
+    private val userVM: UserViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
@@ -59,12 +64,13 @@ class MainActivity : ComponentActivity() {
             //GenericImplementation()
             Column(modifier = Modifier.padding(5.dp)) {
                 Text(
-                    text = "Welcome to the\n Product Gallery: ",
+                    text = "Welcome to the User Gallery: ",
                     fontSize = 30.sp,
                     fontWeight = FontWeight.Bold,
                     textAlign = TextAlign.Center
                 )
-                ProductGallery()
+                //ProductGallery()
+                UserGallery()
             }
         }
 
@@ -119,6 +125,50 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    @Composable
+    fun UserGallery() {
+        val users by userVM.users.observeAsState(emptyList())
+        users.sortedBy { it.username}
+        LazyVerticalGrid(columns = GridCells.Adaptive(128.dp)) {
+            items(users) {
+                UserCard(user=it)
+            }
+        }
+    }
+
+    @Composable
+    fun UserCard(user: User){
+        Card(modifier = Modifier.padding(5.dp), colors = CardDefaults.cardColors(
+            containerColor = Color(0xEEC3F8E8)
+        )) {
+            Column(modifier = Modifier.padding(10.dp)) {
+                Row {
+                    Text(text = "Id: ", fontWeight = FontWeight.Bold)
+                    Text(text = "${user.id}", textDecoration = TextDecoration.Underline)
+                }
+                Row {
+                    Text(text = "Name: ", fontWeight = FontWeight.Bold)
+                    Text(text = "${user.name.firstname} ${user.name.lastname}", textDecoration = TextDecoration.Underline)
+                }
+                Row {
+                    Text(text = "Username: ", fontWeight = FontWeight.Bold)
+                    Text(text = user.username, textDecoration = TextDecoration.Underline)
+                }
+                Row {
+                    Icon(Icons.Default.Email, contentDescription = "Email", modifier = Modifier.size(20.dp, 20.dp))
+                    Text(text = "Email: ", fontWeight = FontWeight.Bold)
+                    Text(text = user.email, textDecoration = TextDecoration.Underline)
+                }
+                Row {
+                    Icon(Icons.Default.Lock, contentDescription = "password", modifier = Modifier.size(20.dp, 20.dp))
+                    Text(text = "Password: ", fontWeight = FontWeight.Bold)
+                    Text(text = user.password, textDecoration = TextDecoration.Underline)
+                }
+            }
+        }
+    }
+
 }
 
 //Product Class
@@ -142,6 +192,9 @@ data class Rating(
 interface ApiService {
     @GET("products")
     suspend fun getProducts(): List<Product>
+
+    @GET("users")
+    suspend fun getUsers(): List<User>
 }
 
 //Product Repository
@@ -182,6 +235,47 @@ class ProductViewModel : ViewModel() {
                 _products.postValue(productList)
 //                println(productList)
             } catch (e: Exception) {
+                println(e.message)
+            }
+        }
+    }
+}
+
+//User class
+data class User(
+    val id:Int,
+    val email: String,
+    val username: String,
+    val password: String,
+    val name: Name
+)
+data class Name(
+    val firstname: String,
+    val lastname: String
+)
+
+class UserRepo(private val apiService: ApiService){
+    suspend fun getUsers():List<User>{
+        return apiService.getUsers()
+    }
+}
+
+class UserViewModel : ViewModel(){
+    private val _users = MutableLiveData<List<User>>()
+    val users:LiveData<List<User>> get() = _users
+
+    private val repo = UserRepo(RetrofitClient.apiService)
+    init {
+        fetchUsers()
+    }
+
+    fun fetchUsers(){
+        viewModelScope.launch {
+            try {
+                val userList = repo.getUsers()
+                _users.postValue(userList)
+                println(userList)
+            }catch (e: Exception){
                 println(e.message)
             }
         }
